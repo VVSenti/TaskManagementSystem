a1eff383b0d8e639aa4206182a10770b2b937555
VVSenti <47742065+VVSenti@users.noreply.github.com>
2024-08-27 20:09:28 +0300

    reques for new JWT

--- src/main/java/ru/sentyurin/TaskManagementSystem/config/SecurityConfig.java
+++ src/main/java/ru/sentyurin/TaskManagementSystem/config/SecurityConfig.java
@@ -2,25 +2,32 @@ package ru.sentyurin.TaskManagementSystem.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import ru.sentyurin.TaskManagementSystem.servicies.PersonDetailsService;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private final PersonDetailsService personDetailsService;
+	private final JWTFilter jwtFilter;
 
 	@Autowired
-	public SecurityConfig(PersonDetailsService personDetailsService) {
+	public SecurityConfig(PersonDetailsService personDetailsService, JWTFilter jwtFilter) {
 		this.personDetailsService = personDetailsService;
+		this.jwtFilter = jwtFilter;
 	}
 
 	// @Override
@@ -29,19 +36,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	// stopping the csrf to make post method work
 	protected void configure(HttpSecurity http) throws Exception {
-//		super.configure(http);
-		http.csrf().disable()
-		.authorizeRequests().antMatchers("/auth/registration").permitAll()
-				.anyRequest().authenticated().and().httpBasic().and().sessionManagement().disable();
-				//.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		//http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+		http.csrf().disable().authorizeRequests().antMatchers("/auth/registration", "/auth/login")
+				.permitAll().anyRequest().authenticated().and().httpBasic().and()
+				.sessionManagement().disable().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 }
--- src/main/java/ru/sentyurin/TaskManagementSystem/controllers/AuthController.java
+++ src/main/java/ru/sentyurin/TaskManagementSystem/controllers/AuthController.java
@@ -6,11 +6,15 @@ import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ru.sentyurin.TaskManagementSystem.dto.AuthenticationDTO;
 import ru.sentyurin.TaskManagementSystem.dto.PersonRegistrationDTO;
 import ru.sentyurin.TaskManagementSystem.models.Person;
 import ru.sentyurin.TaskManagementSystem.security.JWTUtil;
@@ -25,14 +29,16 @@ public class AuthController {
 	private final RegistrationService registrationService;
 	private final JWTUtil jwtUtil;
 	private final ModelMapper modelMapper;
+	private final AuthenticationManager authenticationManager;
 
 	@Autowired
 	public AuthController(PersonValidator personValidator, RegistrationService registrationService,
-			JWTUtil jwtUtil, ModelMapper modelMapper) {
+			JWTUtil jwtUtil, ModelMapper modelMapper, AuthenticationManager authenticationManager) {
 		this.personValidator = personValidator;
 		this.registrationService = registrationService;
 		this.jwtUtil = jwtUtil;
 		this.modelMapper = modelMapper;
+		this.authenticationManager = authenticationManager;
 	}
 
 	@PostMapping("/registration")
@@ -42,18 +48,34 @@ public class AuthController {
 		System.out.println(1);
 		personValidator.validate(person, null);
 		System.out.println(2);
-		
+
 		registrationService.register(person);
 		System.out.println(3);
-		
+
 		String token = jwtUtil.generateToken(person.getEmail());
 		System.out.println(4);
 		return Map.of("jwt-token", token);
 	}
-	
+
+	@PostMapping("/login")
+	public Map<String, String> performLogin(@RequestBody AuthenticationDTO authenticationDTO) {
+
+		UsernamePasswordAuthenticationToken authInputToken = new UsernamePasswordAuthenticationToken(
+				authenticationDTO.getUsername(), authenticationDTO.getPassword());
+		
+		try {
+			authenticationManager.authenticate(authInputToken);
+		} catch (BadCredentialsException e) {
+			return Map.of("message", "Incorrect email or password!");
+		}
+		
+		String token = jwtUtil.generateToken(authenticationDTO.getUsername());
+		return Map.of("jwt-token", token);
+	}
+
 	private Person convertToPerson(PersonRegistrationDTO personDTO) {
 		Person person = modelMapper.map(personDTO, Person.class);
-		
+
 		return person;
 	}
 
